7ce7a42636de5ff9fb873b6c8fa05cc3d8c67238
mohammadafid26 <afid26afid26@gmail.com>
2020-06-29 22:57:26 +0700

    SplashScreen dan Navigation
    SplashScreen dan Navigation

--- app/src/main/java/com/mobile/sipetaniapp/Navigation.java
+++ app/src/main/java/com/mobile/sipetaniapp/Navigation.java
@@ -1,14 +1,64 @@
-package com.mobile.sipetaniapp;
+package com.mobile.sipetani;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.mobile.sipetani.Helper.SharedPreferenceHelper;
 
 public class Navigation extends AppCompatActivity {
+
+    SharedPreferenceHelper sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new BerandaFragment()).commit();
     }
+
+    // Cek apakah belum login pada bagian akun
+    @Override
+    public void onStart(){
+        super.onStart();
+        sp = new SharedPreferenceHelper(this);
+
+        if (!sp.getLogin(true)){
+            startActivity(new Intent(Navigation.this, MainActivity.class));
+            finish();
+        }
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.nav_beranda:
+                    selectedFragment = new BerandaFragment();
+                    break;
+                case R.id.nav_sarana:
+                    selectedFragment = new SaranaFragment();
+                    break;
+                case R.id.nav_akun:
+                    selectedFragment = new AkunFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    selectedFragment).commit();
+
+            return true;
+        }
+    };
 }
--- app/src/main/java/com/mobile/sipetaniapp/SplashScreen.java
+++ app/src/main/java/com/mobile/sipetaniapp/SplashScreen.java
@@ -1,14 +1,48 @@
-package com.mobile.sipetaniapp;
+package com.mobile.sipetani;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.os.Handler;
+        import android.view.animation.Animation;
+        import android.view.animation.AnimationUtils;
+        import android.widget.ImageView;
+        import android.widget.LinearLayout;
+        import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 4000;
+    LinearLayout judul;
+    ImageView logo;
+    TextView deskripsi, developer;
+    Animation frombottom, fromtop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        judul = (LinearLayout)findViewById(R.id.judul);
+        logo = (ImageView)findViewById(R.id.logo);
+        deskripsi = (TextView)findViewById(R.id.deskripsi);
+        developer = (TextView)findViewById(R.id.developer);
+        frombottom = (Animation) AnimationUtils.loadAnimation(this,R.anim.frombottom);
+        fromtop = (Animation) AnimationUtils.loadAnimation(this,R.anim.fromtop);
+
+        judul.setAnimation(fromtop);
+        logo.setAnimation(fromtop);
+        deskripsi.setAnimation(frombottom);
+        developer.setAnimation(frombottom);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splash = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(splash);
+                finish();
+            }
+        },SPLASH_SCREEN);
     }
 }
